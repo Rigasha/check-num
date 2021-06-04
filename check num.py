7d82350894bdb61b0a363a7c256099f2376f62d3
Rigasha <85348908+Rigasha@users.noreply.github.com>
2021-06-04 21:55:52 +0530

    Add files via upload

---
+++ check num.py
@@ -0,0 +1,9 @@
+x_input = input("enter x value\n")
+y_input = input("enter y value\n")
+list_of_scores = x_input.split(":")
+list_of_scores = y_input.split(":")
+
+if (x_input > y_input):
+    print("yes")
+else:
+     print("no")
